77a1eec5d80a3aa3dd0c93561c869e4c417b084b
unknown <aminglbayatpour3@gmail.com>
2022-06-29 15:06:30 +0430

    set up the react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CardProvider } from "./context/card-context";
 
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.scss";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <CardProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </CardProvider>
+  <BrowserRouter>
+    <CardProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </CardProvider>
+  </BrowserRouter>
 );
